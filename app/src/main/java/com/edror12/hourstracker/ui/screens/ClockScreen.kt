a4ea62dc5a59a90fd95aa8bae86728cf53ebca49
Emily Dror <emilydror01@gmail.com>
2026-03-10 23:18:10 +0200

    UI: Add templates for intended screens
    Signed-off-by: Emily Dror <emilydror01@gmail.com>

---
+++ app/src/main/java/com/edror12/hourstracker/ui/screens/ClockScreen.kt
@@ -0,0 +1,28 @@
+package com.edror12.hourstracker.ui.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import com.edror12.hourstracker.ui.theme.HoursTrackerTheme
+
+@Composable
+fun ClockScreen() {
+    Scaffold() { innerPadding ->
+        Text(
+            text = "Placeholder",
+            modifier = Modifier.padding(innerPadding).padding(16.dp)
+        )
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun ClockScreenPreview() {
+    HoursTrackerTheme {
+        ClockScreen()
+    }
+}
