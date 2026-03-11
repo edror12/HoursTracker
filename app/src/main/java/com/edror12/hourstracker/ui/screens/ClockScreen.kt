82092dd4a71ea59a520145a5f41ec29db56a97da
Emily Dror <emilydror01@gmail.com>
2026-03-11 14:00:35 +0200

    import reorganization
    Signed-off-by: Emily Dror <emilydror01@gmail.com>

--- app/src/main/java/com/edror12/hourstracker/ui/screens/ClockScreen.kt
+++ app/src/main/java/com/edror12/hourstracker/ui/screens/ClockScreen.kt
@@ -1,23 +1,18 @@
 package com.edror12.hourstracker.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Scaffold
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
-import androidx.compose.ui.unit.dp
 
+import com.edror12.hourstracker.ui.screens.components.StatsCard
 import com.edror12.hourstracker.ui.screens.components.AnimatedClock
 import com.edror12.hourstracker.ui.screens.components.SquaredButton
-import com.edror12.hourstracker.ui.screens.components.StatsCard
 
 @PreviewScreenSizes
 @Composable
