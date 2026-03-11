0bfb904f0606c746bc4794c8cfe3539d4ed65e5e
Emily Dror <emilydror01@gmail.com>
2026-03-11 14:01:26 +0200

    Remove examples in MainActivity
    Signed-off-by: Emily Dror <emilydror01@gmail.com>

--- app/src/main/java/com/edror12/hourstracker/MainActivity.kt
+++ app/src/main/java/com/edror12/hourstracker/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.edror12.hourstracker
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.edror12.hourstracker.ui.screens.ClockScreen
 import com.edror12.hourstracker.ui.theme.HoursTrackerTheme
 
@@ -20,21 +16,5 @@ class MainActivity : ComponentActivity() {
                 ClockScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HoursTrackerTheme {
-        Greeting("Android")
     }
 }
